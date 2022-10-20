576286f363e4930e7b430f65621419d856f8f5d3
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2022-10-19 23:15:20 -0400

    Change name to emporium-shopping from shopping-list

--- backend/docker-scripts.sh
+++ backend/docker-scripts.sh
@@ -1,3 +1,3 @@
-docker build -t 1239uth/shopping-list-backend:1.0 .
-docker run --name shopping-list-backend -p 8080:8080 1239uth/shopping-list-backend:1.0
-#docker push 1239uth/shopping-list:latest
+docker build -t 1239uth/emporium-shopping-backend:1.0 .
+docker run --name emporium-shopping-backend -p 8080:8080 1239uth/emporium-shopping-backend:1.0
+#docker push 1239uth/emporium-shopping:latest
--- frontend/docker-scripts.sh
+++ frontend/docker-scripts.sh
@@ -1,3 +1,3 @@
-docker build -t 1239uth/shopping-list-frontend:1.0 .
-docker run --name shopping-list-frontend -p 3000:8081 1239uth/shopping-list-frontend:1.0
-#docker push 1239uth/shopping-list:latest
+docker build -t 1239uth/emporium-shopping-frontend:1.0 .
+docker run --name emporium-shopping-frontend -p 3000:8081 1239uth/emporium-shopping-frontend:1.0
+#docker push 1239uth/emporium-shopping:latest
